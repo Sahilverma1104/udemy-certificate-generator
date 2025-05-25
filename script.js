5f0e0c094fb17c98f69139cfd87e020991bf4603
Sahil Verma <155896763+Sahilverma1104@users.noreply.github.com>
2025-05-25 14:54:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function updateCertificate() {
+//   document.getElementById("userName").innerText = document.getElementById("inputName").value;
+  document.getElementById("title").innerText = document.getElementById("inputCourse").value;
+}
